4485251e7da98aa2949de689753511b48846f224
sungsu <22442453+sungsubae@users.noreply.github.com>
2023-01-26 14:51:04 +0900

    [Delete] unwanted testing python file

--- backend/api/testing.py
+++
@@ -1,8 +0,0 @@
-import os
-import pathlib
-
-print(os.path.dirname(os.path.abspath(__file__)))
-print(pathlib.Path(__file__).parent.resolve())
-
-a = pathlib.Path(__file__).parent.resolve()
-print(a/"assests"/"proccessed.csv")
