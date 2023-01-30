57b8aca5878cf3453aa6b7b407bc19e9e559cf32
JWS <dnjstka0307@gmail.com>
2023-01-31 01:45:38 +0900

    [Update] ease.py

--- ml/ease.py
+++ ml/ease.py
@@ -6,6 +6,7 @@ from multiprocessing import Pool, cpu_count
 from google.cloud import bigquery
 from google.oauth2 import service_account
 import requests
+import time
 
 class EASE:
     def __init__(self):
@@ -56,11 +57,21 @@ class EASE:
         df['userid'] = self.user_enc.inverse_transform(df['userid'])
         return df
     
+    def evaluation(train, test): 
+        model = EASE()
+        train = pd.concat([train, test]).reset_index()
+        train['rating'] = 1
+        train.loc[train[train['playtime_forever']<=120].index,'rating'] = 0
+        model.fit(train, 0.5, implicit=False)
+        output = model.evaluate(train, train['userid'].unique(), train['item_id'].unique(), 10)
+        evale = pd.merge(train[train['rating']==1], output, on=['userid','item_id'], how='inner')
+        precision10 = (evale.groupby('userid').count()['index']/10).values.mean()
+        return precision10
     
     @staticmethod
     def predict_for_user(user, group, pred, items, k):
         watched = set(group['ci'])
-        candidates = [item for item in items]
+        candidates = [item for item in items if item not in watched]
         pred = np.take(pred, candidates)
         res = np.argpartition(pred, -k)[-k:]
         r = pd.DataFrame(
@@ -73,33 +84,26 @@ class EASE:
         return r
     
 def dataload():
-    credential_path = '/opt/ml/input/final_project/ml/key.json'
+    credential_path = 'key.json'
     credentials = service_account.Credentials.from_service_account_file(credential_path)
     client = bigquery.Client(credentials=credentials, project=credentials.project_id)
 
-    q="SELECT DISTINCT * FROM `data.game`"
+    q="SELECT DISTINCT App_ID, Genre, Positive_Reviews, Negative_Reviews FROM `data.game`"
     game = client.query(q).to_dataframe()
     
-    q="SELECT DISTINCT * FROM `data.interaction`"
+    q="SELECT DISTINCT item_id, userid, playtime_forever FROM `data.new_interaction`"
     data = client.query(q).to_dataframe()
     
-    data = data[['userid','item_id','playtime_forever']]
-    c = data.groupby('userid').count().sort_values(by='item_id')
-    
-    data_20 = c[(c['item_id']<=20)].reset_index()
-    data_20 = data[data['userid'].isin(data_20['userid'])]
-    
-    return data_20, game
+    return data, game
 
     
-def get_user(userid):
-    input_ = requests.get(f'https://api.steampowered.com/IPlayerService/GetOwnedGames/v1/?key=F644C6E2851DBDC938FF6FC70388451F&steamid={userid}&include_played_free_games=True&include_appinfo=True')
+def get_user(userid,api):
+    input_ = requests.get(f'https://api.steampowered.com/IPlayerService/GetOwnedGames/v1/?key={api}&steamid={userid}&include_played_free_games=True&include_appinfo=True')
     test = pd.DataFrame(input_.json()['response']['games'])
     test = test[['appid','playtime_forever']]
-    test['userid'] = 'JWS'
+    test['userid'] = userid
     test.columns = ['item_id','playtime_forever' ,'userid']
     return test
-
 
 def train_predict(train, test, game): 
     model = EASE()
@@ -108,18 +112,16 @@ def train_predict(train, test, game):
     train.loc[train[train['playtime_forever']<=120].index,'rating'] = 0
     model.fit(train, 0.5, implicit=False)
     output = model.predict(test, test['userid'].unique(), train['item_id'].unique(), 50)
-    output = pd.merge(game, output, left_on='App_ID', right_on='item_id', how='inner')
-    output = output[(output['Genre'].str.contains('Indie', na=False)) & (output['Positive_Reviews']>=output['Negative_Reviews']*4) & (output['Positive_Reviews']+output['Negative_Reviews']<=50000)][['App_ID', 'Name']]
-    
-    return output['App_ID'].values
+    list_ = game[(game['Genre'].str.contains('Indie', na=False)) & (game['Positive_Reviews']>=game['Negative_Reviews']*4) & (game['Positive_Reviews']+game['Negative_Reviews']<=50000)]['App_ID'].values
+    output = output[output['item_id'].isin(list_)]['item_id'].values
+    return output
 
-def main(userid=76561198117856251):
+def main():
+    userid=76561198117856251
     train, game = dataload()
     test = get_user(userid) 
     output = train_predict(train, test, game)
-    
     return output
 
 if __name__ == "__main__":
     main()
-    
