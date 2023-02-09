402e1862c27b25553f2ebd33cd5488ca0c2595c6
sungsu <22442453+sungsubae@users.noreply.github.com>
2023-02-09 01:10:06 +0000

    [Add] CORS middleware to main.py

--- backend/api/main.py
+++ backend/api/main.py
@@ -1,8 +1,10 @@
 import pathlib
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import FileResponse
 
+from core.setting import config
 from routers import user, login, game
 
 PATH = pathlib.Path(__file__).parent.resolve()
@@ -11,6 +13,15 @@ app.include_router(user.router)
 app.include_router(login.router)
 app.include_router(game.router)
 
+# front http://ip:port 추가
+origins = []
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 @app.get("/")
 def get_root():
